ffb2f8eb11a494bcad2ccd0add135674efecce37
Emre Yavuz <emreeyavuzz98@gmail.com>
2024-02-05 23:23:08 +0300

    task 3

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,7 +3,10 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba");
-        System.out.println("Merhaba 2");
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Merhaba 5");
+        }
 
 
         for (int i = 0; i < 5; i++) {
