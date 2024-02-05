324f43683f5044d770428e3dd8b1aee38791fe23
Emre Yavuz <emreeyavuzz98@gmail.com>
2024-02-05 23:18:59 +0300

    Satın alma işlemi onayı eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,7 +4,11 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Merhaba 2");
-        System.out.println("Merhaba 3");
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Merhaba");
+        }
 
     }
 }
