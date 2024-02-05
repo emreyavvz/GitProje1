8f1fa50189f660dd2f457755cc1770a86858c325
Emre Yavuz <emreeyavuzz98@gmail.com>
2024-02-05 23:14:49 +0300

    Kredi Kartı Kontrolü eklendi

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,6 +4,7 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Merhaba 2");
+        System.out.println("Merhaba 3");
 
     }
 }
